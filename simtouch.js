4b7d11348df62e743fba6b45976355e45f5235cf
Lennholm <miklen@lensway.com>
2013-08-04 17:39:28 +0200

    Syntax

--- simtouch.js
+++ simtouch.js
@@ -1,23 +1,23 @@
-;(function(){
+;(function(document, window){
     var button = document.createElement("button"),
         active = false,
         touchmove = false,
         mappings = {mousedown: "touchstart", mouseup: "touchend", mousemove: "touchmove"},
         interceptions = ["mouseover","mouseout","mouseenter","mouseleave","mousemove","mousedown","mouseup","click"];
 
-    button.innerHTML="Touch";
-    button.style.backgroundColor="#ccc";
-    button.style.position="fixed";
-    button.style.left="5px";
-    button.style.top="5px";
-    button.disabled= !window.addEventListener || !document.createEvent;
+    button.innerHTML = "Touch";
+    button.style.backgroundColor = "#ccc";
+    button.style.position = "fixed";
+    button.style.left = "5px";
+    button.style.top = "5px";
+    button.disabled = !window.addEventListener || !document.createEvent;
 
     document.addEventListener("DOMContentLoaded", function(){
         document.body.appendChild(button);
-    },false);
+    }, false);
     window.addEventListener("load", function(){
         document.body.appendChild(button);
-    },false);
+    }, false);
     function intercept(event){
         event.stopPropagation();
         event.preventDefault();
@@ -28,27 +28,30 @@
         } else if (mappings[event.type]){
             var dEvt = document.createEvent("HTMLEvents");
             dEvt.initEvent(mappings[event.type], true, true);
-            dEvt.touches = [{pageX: event.pageX, pageY: event.pageY}];
+            dEvt.touches = [{
+                pageX: event.pageX,
+                pageY: event.pageY
+            }];
             event.target.dispatchEvent(dEvt);
-            if (event.type=="mousedown")
+            if (event.type == "mousedown")
                 touchmove = true;
-            if (event.type=="mouseup")
+            if (event.type == "mouseup")
                 touchmove = false;
         }
     }
     function toggleSim(){
         var i = 0, action = active ? "remove" : "add";
         if (active){
-            button.style.backgroundColor="#ccc";
-            document.body.style.cursor="";
+            button.style.backgroundColor = "#ccc";
+            document.body.style.cursor = "";
         } else {
-            button.style.backgroundColor="green";
-            document.body.style.cursor="pointer";
+            button.style.backgroundColor = "green";
+            document.body.style.cursor = "pointer";
         }
-        for (;i < interceptions.length; i++){
-            document.body[action+"EventListener"](interceptions[i],intercept,true);
+        for (; i < interceptions.length; i++){
+            document.body[action + "EventListener"](interceptions[i], intercept, true);
         }
         active = !active;
     }
     button.addEventListener("click", toggleSim, false);
-})();
+})(document, window);
